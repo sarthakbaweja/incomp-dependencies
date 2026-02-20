9009252c051378fe1f8379454a77546984aa27ba
sarthakbaweja <sarthak.baweja@splashlearn.com>
2026-02-20 23:42:38 +0530

    added imports

--- src/incomp_depend/main.py
+++ src/incomp_depend/main.py
@@ -1,9 +1,14 @@
 """Main module for incomp-depend."""
 
+import tensorflow as tf
+import protobuf
+
 
 def main():
     """Entry point for the application."""
     print("Hello from incomp-depend!")
+    print(f"TensorFlow version: {tf.__version__}")
+    print(f"Protobuf version: {protobuf.__version__}")
 
 
 if __name__ == "__main__":
