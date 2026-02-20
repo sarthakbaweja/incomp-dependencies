445ee9c3542347f3ad4d0c18ecbbae5201b6c70d
sarthakbaweja <sarthak.baweja@splashlearn.com>
2026-02-20 23:48:37 +0530

    new imports

--- src/incomp_depend/main.py
+++ src/incomp_depend/main.py
@@ -2,6 +2,8 @@
 
 import tensorflow as tf
 import protobuf
+from prophet import Prophet
+from PyPDF2 import PdfReader
 
 
 def main():
@@ -9,6 +11,8 @@ def main():
     print("Hello from incomp-depend!")
     print(f"TensorFlow version: {tf.__version__}")
     print(f"Protobuf version: {protobuf.__version__}")
+    print(f"Prophet loaded: {Prophet.__name__}")
+    print(f"PyPDF2 PdfReader loaded: {PdfReader.__name__}")
 
 
 if __name__ == "__main__":
